c9ce5ba82c32cc0d96104256d6be2fccc8300d8e
[Hazem-Gamall] <[hazemgamalelgendy@gmail.com]>
2020-08-06 13:59:25 +0200

    plot the data into meaningful charts

---
+++ plotData.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+grades = []
+with open('science.txt','r') as f:
+     f.readline()
+     f.readline()
+     s = f.read().splitlines()
+     for i in s:
+         l = i.split(',')
+         if i != '':
+            grades.append(l[0])
+
+grades = list(map(float,grades))
+print(grades)
+
+plt.hist(grades, bins='auto')
+plt.show()
